9f24e28b0ce586ebd03fb437ade9f89d06c50c06
Tibebeselasie Mehari <tibebes.js@gmail.com>
2019-02-26 08:15:38 -0500

    move some methods from employee interface into a separate interface

--- src/Employee.java
+++ src/Employee.java
@@ -3,11 +3,5 @@ interface Employee{
     private void setSalary(double salary);
     public String getName();
     public double getSalary();
-    public double getControlSpanSalary();
-    public void addSubordinate(Employee e);
-    public void removeSubordinate(Employee e);
-    public Employee getSubordinate(int i);
-    public void print();
-    // could also go and create getAllSubordinates Etc..
 }
 
---
+++ src/EmployeeBoss.java
@@ -0,0 +1,7 @@
+public interface EmployeeBoss{
+    public double getControlSpanSalary();
+    public void addSubordinate(Employee e);
+    public void removeSubordinate(Employee e);
+    public Employee getSubordinate(int i);
+    // could also go and create getAllSubordinates Etc..
+}
