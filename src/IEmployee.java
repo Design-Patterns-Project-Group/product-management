ab823047fb46d58ce1da55052a3517787e61e7d8
Tibebeselasie Mehari <tibebes.js@gmail.com>
2019-02-26 08:21:46 -0500

    added a comment

--- src/IEmployee.java
+++ src/IEmployee.java
@@ -1,5 +1,5 @@
 interface Employee{
-    private void setName(String name);
+    private void setName(String name); // not the person's name rather the position
     private void setSalary(double salary);
     public String getName();
     public double getSalary();
