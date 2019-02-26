b3490cfa729206c2b4f016a5e68222039de16a00
Tibebeselasie Mehari <tibebes.js@gmail.com>
2019-02-26 08:08:27 -0500

    added private set methods

--- src/Employee.java
+++ src/Employee.java
@@ -1,6 +1,8 @@
 interface Employee{
+    private void setName(String name);
+    private void setSalary(double salary);
     public String getName();
-    public float getSalary();
+    public double getSalary();
     public double getControlSpanSalary();
     public void addSubordinate(Employee e);
     public void removeSubordinate(Employee e);
